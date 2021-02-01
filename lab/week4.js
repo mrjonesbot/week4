6c3ee4613696dbb98fc6f168b2095014d7c2bb79
Nathan Jones <nate@mrjones.io>
2021-01-31 18:08:12 -0600

    lab solution

--- lab/week4.js
+++ lab/week4.js
@@ -18,10 +18,47 @@ async function pageLoaded() {
 
   // writes the returned JSON to the console
   console.log(json)
-  
-  // 🔥 start here: write code to loop through the products; each
+
+  // 1. get the data you are working with
+  let products = json.products
+  // console.log(products)
+
+  let renderProduct = function(product) {
+    return `
+    <div class="p-4 w-full md:w-1/2 lg:w-1/3">
+      <div class="border h-full p-4 flex flex-col">
+        <h2 class="text-lg font-bold mb-4">${product.name}</h2>
+        <div class="mb-4"><img src="${product.image}">
+        </div>
+        <div class="mb-4 text-gray-900">
+          ${product.description}
+        </div>
+        <div class="mt-auto text-purple-500 text-2xl">$${product.price}</div>
+      </div>
+    </div>
+    `
+  }
+
+  for(let index = 0; index < products.length; index++) {
+    let product = products[index]
+    let element = document.querySelector('.products')
+    // let welcomeMessageElement = document.querySelector('.welcome-message')
+
+    console.log(welcomeMessageElement)
+
+    // welcomeMessageElement.insertAdjacentHTML('afterbegin', "BLAH ")
+
+    element.insertAdjacentHTML('beforeend', renderProduct(product))
+    // element.insertAdjacentHTML("beforebegin", render)
+    // element.insertAdjacentHTML("afterend", render)
+  }
+
+    
+
+
+  // 🔥 start here: ; each
   // iteration of the loop should call your renderProduct function
-  // make it work first; then extract to the separate renderProduct
+  // ; 
   // function after it's 100% working without it
   
 }
